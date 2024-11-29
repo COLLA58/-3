134a38b27c009b3678c423686a83d555f4ebb8a2
AAAAAAAAAA <colahola36@gmail.com>
2024-11-29 14:00:38 +0400

    111

--- a=input().py
+++ a=input().py
@@ -1,4 +1,4 @@
-import randoma
+import random
 
 rundom_nambers = [random.randint(1,10000) for i in range(10)]
 print(rundom_nambers)
@@ -7,6 +7,7 @@ print(min(rundom_nambers))
 print(max(rundom_nambers))
 
 sum = 0
+#####
 
 for i in rundom_nambers:
     sum += i
